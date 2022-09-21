94727dfdf0fd355b0f6d2628527fba023818b07c
Dannyntale10 <dannyntale10@gmail.com>
2022-09-21 12:50:46 -0700

    0. strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - concatenates two strings
+ * @dest: string
+ * @src: string to add
+ *
+ * Return: the resulting string
+*/
+char *_strcat(char *dest, char *src);
+{
+	int i, len 1, len 2;
+
+	char *dest[30] = len 1;
+	char *src[] = len 2;
+
+	for (i = 0, i <= char *src; i++)
+	{
+		char *dest[len 1 + i] = char *src[i];
+	}
+
+	_putchar ("string after concatenation is %s\n", len 1);
+
+	return (dest);
+}
