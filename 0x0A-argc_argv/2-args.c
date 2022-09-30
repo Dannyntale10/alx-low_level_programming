d9ea354b82a168f4d50072abdeb25ce1d1ea45e7
Dannyntale10 <dannyntale10@gmail.com>
2022-09-30 05:45:40 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -5,15 +5,14 @@
  * @argc: Number of command line arguments
  * @argv: Array name
  * Return: 0
- *
-*/
+ */
 
 int main(int argc, char *argv[])
 {
 	int i;
 
-	for (i = 0; i < argc; i ++)
-		printf("%s\n", argv[i]);
+	for (i = 0; i < argc; i++)
+	printf("%s\n", argv[i]);
 
 	return (0);
 }
